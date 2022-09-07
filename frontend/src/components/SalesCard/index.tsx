37466a6fae392cdb76a7c0ce058119ef5b6b4e47
leandbs2022 <109764459+leandbs2022@users.noreply.github.com>
2022-09-07 11:38:16 -0300

    Security config

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -5,17 +5,13 @@ import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import './styles.css'
 
-
-
-
 function SalesCard() {
-
     const min = new Date(new Date().setDate(new Date().getDate() - 365))
     const max = new Date()
 
-    const[ minDate, setMinDate] = useState(min)
+    const [minDate, setMinDate] = useState(min)
     const [maxDate, setMaxDate] = useState(max)
-   
+
     return (
         <>
             <div className="dsmeta-card">
